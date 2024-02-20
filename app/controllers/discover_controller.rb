71910910a42e931b670c674cbcb2faf412a606d1
adrianm255 <95966222+adrianm255@users.noreply.github.com>
2024-02-20 14:36:43 +0200

    small optimizations

--- app/controllers/discover_controller.rb
+++ app/controllers/discover_controller.rb
@@ -2,6 +2,6 @@ class DiscoverController < ApplicationController
   layout 'courses'
 
   def index
-    @courses = Course.where(published: true).order(created_at: :desc)
+    @courses = Course.where(published: true).order(created_at: :desc).as_json(include: { author: { only: %i[id email] } })
   end
 end
